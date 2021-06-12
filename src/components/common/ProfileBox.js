365eea0d7e3d168a1f33c684febbfeb55b0d164e
afzal <saiyedafzalgz@gmail.com>
2021-06-12 05:05:02 -0700

    added Navbar

--- src/components/common/ProfileBox.js
+++ src/components/common/ProfileBox.js
@@ -1,7 +1,20 @@
+import style from '../../style/main-layout.module.scss';
 const ProfileBox = () => {
     return (
-    <div>
-        
+    <div className={`${style['nav-right-item']} ${style['profile-box']}`}>
+        <div className={style['profile-box-container']}>
+            <div className={style['opener-icon']}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={style['profile-image']}>
+                <img src="/default-profile.png" alt="default-pic" width={30}/>
+            </div>
+        </div>
+        <div style={style['profile-box-dropdown']}>
+
+        </div>
     </div>
     );
 };
