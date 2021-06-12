dd61202a089100cdc89968921271371a67773766
afzal <saiyedafzalgz@gmail.com>
2021-06-12 07:24:38 -0700

    made profilebox fully functional

--- src/components/common/ProfileBox.js
+++ src/components/common/ProfileBox.js
@@ -1,7 +1,26 @@
 import style from '../../style/main-layout.module.scss';
+import { useState, useEffect } from 'react'
 const ProfileBox = () => {
+
+    const [dropDownActive, setDropDownActive] = useState(false)
+    const toggleDropDown = () => {
+        setDropDownActive(s => !s)
+    }
+    useEffect(() => {
+        const deactivateDropDown = () => {
+            if (dropDownActive){
+                setDropDownActive(false)
+            }
+            
+        }
+        document.body.addEventListener('click', deactivateDropDown)
+
+        return () => {
+            document.body.removeEventListener('click', deactivateDropDown)
+        }
+    }, [dropDownActive])
     return (
-    <div className={`${style['nav-right-item']} ${style['profile-box']}`}>
+    <div className={`${style['nav-right-item']} ${style['profile-box']}`} onClick={toggleDropDown}>
         <div className={style['profile-box-container']}>
             <div className={style['opener-icon']}>
                 <span></span>
@@ -12,8 +31,12 @@ const ProfileBox = () => {
                 <img src="/default-profile.png" alt="default-pic" width={30}/>
             </div>
         </div>
-        <div style={style['profile-box-dropdown']}>
-
+        <div className={`${style['profile-box-dropdown']} ${ dropDownActive && style['profilebox-active']}`}>
+            <ul className={style['profile-box-dropdown-list']}>
+                <li className={style['profile-box-dropdown-item']}>Profile</li>
+                <li className={style['profile-box-dropdown-item']}>Settings</li>
+                <li className={style['profile-box-dropdown-item']}>Logout</li>
+            </ul>
         </div>
     </div>
     );
