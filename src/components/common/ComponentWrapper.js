25c29bdba3842793ba0fa448dedec969b7580089
afzal <saiyedafzalgz@gmail.com>
2021-06-14 08:02:03 -0700

    added Custom scrollbar

--- src/components/common/ComponentWrapper.js
+++ src/components/common/ComponentWrapper.js
@@ -4,6 +4,22 @@ const {className, styles} = css.resolve`
     .component-wrapper {
         margin: 20px 0px;
         overflow-x: auto;
+        scrollbar-width: thin;          
+        scrollbar-color: black; 
+    }
+    .component-wrapper::-webkit-scrollbar {
+        
+        height: 6px;
+    }
+    .component-wrapper::-webkit-scrollbar-track {
+        background: white;
+        border: 1px solid black;
+        border-radius: 20px;
+    }
+    .component-wrapper::-webkit-scrollbar-thumb {
+        background-color: black;  
+        border-radius: 20px;      
+        
     }
 `
 const ComponentWrapper = ({ children }) => {
