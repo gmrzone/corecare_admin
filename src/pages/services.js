85f01abc8f15990b634c1ba5ff10c31147f729b5
afzal <saiyedafzalgz@gmail.com>
2021-06-17 01:19:41 -0700

    added dummy data for subcategory

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,9 +1,93 @@
 import MainLayout from "../components/common/MainLayout";
 import CreateAction from '../components/common/CreateAction'
+import ListTable from '../components/common/ListTable'
+import ComponentWrapper from '../components/common/ComponentWrapper'
 const Services = () => {
+    const tableData = [
+        {
+            order_id: 1234546,
+            status: "Fullfilled",
+            user: "AFzal saiyed",
+            total: "1000.93",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 2234596,
+            status: "Pending",
+            total: "2600.56",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 3123654,
+            status: "Fullfilled",
+            total: "1100.00",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 4456321,
+            status: "Fullfilled",
+            total: "4300.93",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 5856324,
+            status: "Pending",
+            total: "934.34",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 6963254,
+            status: "Fullfilled",
+            total: "53000.50",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        },
+        {
+            order_id: 7963254,
+            status: "Fullfilled",
+            total: "3890.02",
+            user: "AFzal saiyed",
+            created: "1 Day ago",
+            
+        }
+    ]
+    const tableHead = [
+        "Order ID",
+        "User",
+        "Status",
+        "Total",
+        "Created"
+
+    ]
+    const tableBody = tableData.map((x) => {
+        return  (
+            <tr style={{cursor: 'pointer'}} key={x.order_id}>
+                <td>{x.order_id}</td>
+                <td>{x.user}</td>
+                <td>{x.status}</td>
+                <td>{x.total}</td>
+                <td>{x.created}</td>
+            </tr>
+        )
+    })
     return (
         <MainLayout>
-            <CreateAction forPage="Service"/>
+            <ComponentWrapper>
+                <CreateAction forPage="Service"/>
+            </ComponentWrapper>
+            <ComponentWrapper>
+                <ListTable headData={tableHead} bodyData={tableBody}/>
+            </ComponentWrapper>
         </MainLayout>
     )
 }
--- src/pages/subcategory.js
+++ src/pages/subcategory.js
@@ -1,9 +1,75 @@
 import MainLayout from "../components/common/MainLayout";
 import CreateAction from '../components/common/CreateAction'
+import ListTable from '../components/common/ListTable'
+import ComponentWrapper from '../components/common/ComponentWrapper'
 const Subcategory = () => {
+    const tableData = [
+        {
+            name: "AC Service Consultation",
+            category: "AC Expert",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Wallpaper / Stencils",
+            category: "Painter",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Sofa Shampooing",
+            category: "Cleaner",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Furniture Assembly",
+            category: "Carpenter",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Minor Installation",
+            category: "Plumber",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Room Heater",
+            category: "Electrician",
+            created: "1 Day ago",
+            
+        },
+        {
+            name: "Switch/Socket",
+            category: "Electrician",
+            created: "1 Day ago",
+            
+        }
+    ]
+    const tableHead = [
+        "Name",
+        "Category",
+        "Created"
+
+    ]
+    const tableBody = tableData.map((x) => {
+        return  (
+            <tr style={{cursor: 'pointer'}} key={x.order_id}>
+                <td>{x.name}</td>
+                <td>{x.category}</td>
+                <td>{x.created}</td>
+            </tr>
+        )
+    })
     return (
         <MainLayout>
-            <CreateAction forPage="Subcategory"/>
+             <ComponentWrapper>
+                <CreateAction forPage="Subcategory"/>
+             </ComponentWrapper>
+             <ComponentWrapper>
+                <ListTable headData={tableHead} bodyData={tableBody}/>
+             </ComponentWrapper>
         </MainLayout>
     )
 }
