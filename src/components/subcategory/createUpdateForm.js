d3abd17dd1ccc74a6229eb1bab5c679c1dfe672a
afzal saiyed <saiyedafzalgz@gmail.com>
2021-06-24 13:28:14 +0530

    added subcategory Create and update form

--- src/components/subcategory/createUpdateForm.js
+++ src/components/subcategory/createUpdateForm.js
@@ -1,6 +1,42 @@
-const createUpdateForm = () => {
+const createUpdateForm = ({register, formErrors, serverErrors}) => {
     return (
         <>
+            <div className={`field ${formErrors.name && "error"}`}>
+                <label>Name</label>
+                <input
+                    type="text"
+                    placeholder="name"
+                    {...register("name", {
+                        required: { value: true, message: "Cannot create or Update a subcategory without a name" },
+                        minLength: { value: 3, message: "Subcategory cannot be less then 3 Char" },
+                    })}
+                />
+            </div>
+            <div className={`field ${formErrors.slug && "error"}`}>
+                <label>Name</label>
+                <input
+                    type="text"
+                    placeholder="slug"
+                    {...register("slug")}
+                />
+            </div>
+            <div className={`field ${formErrors.service_specialist && "error"}`}>
+                <label>Employee Category</label>
+                <select className="ui fluid dropdown" {...register('service_specialist', {required: {value: true, message: "Employee Category is required"}})}>
+                    <option value="">Select Category</option>
+                    <option value="electrician">Electrician</option>
+                    <option value="carpenter">Carpenter</option>
+                    <option value="plumber">Plumber</option>
+                    <option value="cleaner">Cleaner</option>
+                    <option value="painter">Painter</option>
+                    <option value="appliance">Appliance Expert</option>
+                    <option value="pest-control">Pest-Control</option>
+                    <option value="ac-service">AC Expert</option>
+                </select>
+            </div>
+            <div className={`ui red message ${formErrors.name || formErrors.service_specialist || serverErrors ? "visible" : "hidden"}`}>
+                    {formErrors.name?.message || formErrors.service_specialist?.message || serverErrors}
+            </div>
         </>
     )
 }
--- src/pages/subcategory.js
+++ src/pages/subcategory.js
@@ -3,51 +3,51 @@ import CreateAction from "../components/common/CreateAction";
 import ListTable from "../components/common/ListTable";
 import ComponentWrapper from "../components/common/ComponentWrapper";
 import DateRangePicker from "../components/common/DateRangeFilter";
-import Modal from '../components/common/Modal'
-import CreateUpdateForm from '../components/subcategory/createUpdateForm'
+import Modal from "../components/common/Modal";
+import CreateUpdateForm from "../components/subcategory/createUpdateForm";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 const Subcategory = () => {
     const tableData = [
         {
             name: "AC Service Consultation",
-            category: "AC Expert",
+            service_specialist: "Ac-Service",
             created: "1 Day ago",
         },
         {
             name: "Wallpaper / Stencils",
-            category: "Painter",
+            service_specialist: "Painter",
             created: "1 Day ago",
         },
         {
             name: "Sofa Shampooing",
-            category: "Cleaner",
+            service_specialist: "Cleaner",
             created: "1 Day ago",
         },
         {
             name: "Furniture Assembly",
-            category: "Carpenter",
+            service_specialist: "Carpenter",
             created: "1 Day ago",
         },
         {
             name: "Minor Installation",
-            category: "Plumber",
+            service_specialist: "Plumber",
             created: "1 Day ago",
         },
         {
             name: "Room Heater",
-            category: "Electrician",
+            service_specialist: "Electrician",
             created: "1 Day ago",
         },
         {
             name: "Switch/Socket",
-            category: "Electrician",
+            service_specialist: "Electrician",
             created: "1 Day ago",
         },
     ];
 
-    const [modalActive, setModalActive] = useState(false)
-    const [formType, setFormType] = useState({type: null, header: null, error: null})
+    const [modalActive, setModalActive] = useState(false);
+    const [formType, setFormType] = useState({ type: null, header: null, error: null });
     const {
         register,
         setValue,
@@ -56,33 +56,58 @@ const Subcategory = () => {
     } = useForm();
 
     const openCreateModal = () => {
-        setFormType({type: 'create', header: "Update Subcategory"})
-        setModalActive(true)
-    }
-    const openUpdateModal = () => {
-        setFormType({type: 'update', header: "Update Subcategory"})
-        setModalActive(true)
-    }
+        setFormType({ type: "create", header: "Create Subcategory" });
+        setValue("name", "", { shouldValidate: false });
+        setValue("service_specialist", "", { shouldValidate: false });
+        setValue("slug", "", { shouldValidate: false });
+        setModalActive(true);
+    };
+    const openUpdateModal = (name, service_specialist, slug) => {
+        setFormType({ type: "update", header: "Update Subcategory" });
+        setValue("name", name, { shouldValidate: false });
+        setValue("service_specialist", service_specialist, { shouldValidate: false });
+        setValue("slug", slug, { shouldValidate: false });
+        setModalActive(true);
+    };
 
     const closeModal = () => {
         setModalActive(false);
     };
-    const onSubmit = () => {
-
-    }
+    const onSubmit = (formValues, e) => {
+        e.target.reset();
+        setFormType((s) => {
+            return { ...s, error: "You are not authorized to use this form please contact Super User." };
+        });
+        console.log(formValues);
+    };
     const tableHead = ["Name", "Category", "Created"];
     const tableBody = tableData.map((x, i) => {
         return (
-            <tr style={{ cursor: "pointer" }} key={i} onClick={openUpdateModal}>
+            <tr
+                style={{ cursor: "pointer" }}
+                key={i}
+                onClick={() =>
+                    openUpdateModal(
+                        x.name,
+                        x.service_specialist.toLowerCase(),
+                        x.name.replace(/(\/)/gi, "-").replace(" ", "-").toLowerCase(),
+                    )
+                }>
                 <td>{x.name}</td>
-                <td>{x.category}</td>
+                <td>{x.service_specialist}</td>
                 <td>{x.created}</td>
             </tr>
         );
     });
     return (
         <MainLayout>
-            <Modal active={modalActive} header={formType.header} closeModal={closeModal} submitFor={onSubmit} handleSubmit={handleSubmit}>
+                <Modal
+                header={formType.header}
+                active={modalActive}
+                closeModal={closeModal}
+                submitForm={onSubmit}
+                handleSubmit={handleSubmit}
+                formError={formErrors}>
                 <CreateUpdateForm register={register} formErrors={formErrors} serverErrors={formType.error} />
             </Modal>
             <ComponentWrapper>
